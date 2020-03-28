bfdab54e214864b5eef61615a7dddb95610674a3
Raphael Marco <raphaelmarco@outlook.com>
2020-03-28 19:15:38 +0800

    Add cron

---
+++ app/services/cron.js
@@ -0,0 +1,7 @@
+const cron = require('node-cron');
+
+exports.run = () => {
+  // cron.schedule('*/5 * * * * *', () => {
+  //   console.log('hello');
+  // });
+};
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const bootstrap = require('./bootstrap');
 const server = require('./server');
+const cron = require('@services/cron');
 
 process.on('unhandledRejection', (err) => {
   process.stderr.write(`${err}\n`);
   process.exit(1);
 });
 
-bootstrap();
+bootstrap().then(() => {
+  cron.run();
 
-server.start();
+  server.start();
+});

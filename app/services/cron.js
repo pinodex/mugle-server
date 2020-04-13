a0147c9eaeec851bb897d0f32da54faedb9ffefa
Raphael Marco <raphaelmarco@outlook.com>
2020-04-13 16:50:06 +0800

    Run Twilio import only in production env setting

--- app/services/cron.js
+++ app/services/cron.js
@@ -1,13 +1,16 @@
 const cron = require('node-cron');
 const findMatches = require('@tasks/find-matches');
 const importTwilioNtsIce = require('@tasks/import-twilio-nts-ice');
+const { env } = require('@config/app');
 
 exports.run = (server) => {
   cron.schedule('*/1 * * * * *', () => {
     findMatches(server);
   });
 
-  cron.schedule('0 */12 * * *', () => {
-    importTwilioNtsIce();
-  });
+  if (env === 'production') {
+    cron.schedule('0 */12 * * *', () => {
+      importTwilioNtsIce();
+    });
+  }
 };

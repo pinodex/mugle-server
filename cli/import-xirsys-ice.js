7bbea95a44ba2e7012734a3c75848480603c5be0
Raphael Marco <raphaelmarco@outlook.com>
2020-04-13 16:46:20 +0800

    Remove Xirsys ICE seeder

--- cli/import-xirsys-ice.js
+++
@@ -1,8 +0,0 @@
-const bootstrap = require('../bootstrap');
-const importXirsysIce = require('@tasks/import-xirsys-ice');
-
-bootstrap().then(async () => {
-  await importXirsysIce();
-
-  process.exit(0);
-});
--- config/xirsys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  username: process.env.XIRSYS_USERNAME,
-  password: process.env.XIRSYS_PASSWORD,
-  channel: process.env.XIRSYS_CHANNEL,
-};
